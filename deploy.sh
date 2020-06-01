b8feeb526786e3f5b9a4945e8e13e3fb9bf8baa3
hejin <hejinpy@163.com>
2020-06-02 00:56:12 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+tar -czf build.tar.gz build
+scp build.tar.gz mirror-riddle.xyz:/tmp
+rm build.tar.gz
+ssh -t mirror-riddle.xyz 'cd /tmp; tar -xzf build.tar.gz; sudo mv build /data/www/mirror-riddle-docs'
