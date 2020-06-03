867b09eb2f3d198bc21f2df0852159cd50c65f43
hejin <hejinpy@163.com>
2020-06-03 09:51:17 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
+git pull
 yarn build
-tar -czf build.tar.gz build
-scp build.tar.gz mirror-riddle.xyz:/tmp
-rm build.tar.gz
-ssh -t mirror-riddle.xyz 'cd /tmp; tar -xzf build.tar.gz; sudo rm -r /data/www/mirror-riddle-docs; sudo mv build /data/www/mirror-riddle-docs'
+sudo rm -r /data/www/mirror-riddle-docs
+sudo cp -r build /data/www/mirror-riddle-docs
