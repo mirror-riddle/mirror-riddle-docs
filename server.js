03b6b673a8d8bfab6c83ca9100937fac6a6bdc53
hejin <hejinpy@163.com>
2020-06-03 11:06:14 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const http = require('http');
 const express = require('express');
 const childProcess = require('child_process');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+path.join(__dirname, 'deploy.sh');
 
 const app = express();
-app.post('/', (req, res) => {
-  console.log(req.body, res.header);
-  childProcess.execFile('./deploy.sh');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/').post((req, res) => {
+  console.log(req.body, res.headers);
+  childProcess.execFile('./deploy.sh', [], {}, (error, stdout, stderr) => {
+    if (err) {
+      throw err;
+    }
+    console.log(stdout);
+  });
 });
 
 http.createServer(app).listen(3100);
